816ef102523becab350da64602602668c3515490
jeanne desmier <desmierjeanne@gmail.com>
2022-08-23 17:09:42 +0200

    page home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
 skip_before_action :authenticate_user!, only: :home
 
   def home
+    @amines = Amine.order('RANDOM()').limit(3)
+    @amine = Amine.where("")
   end
 end
