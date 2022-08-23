b0da875e520b45a27b58d255ef5e310515b1e59f
jeanne desmier <desmierjeanne@gmail.com>
2022-08-23 11:23:28 +0200

    controller amine

---
+++ app/controllers/amines_controller.rb
@@ -0,0 +1,30 @@
+class AminesController < ApplicationController
+
+  def search
+    @amines = Amine.where("name LIKE ? or mood LIKE ? or category LIKE ?", "%"+ params[:query] +"%")
+  end
+
+  def new
+    @amine = Amine.new
+  end
+
+  def create
+    @amine = Amine.new(amine_params)
+    @amine.owner = current_user
+    if @amine.save
+      redirect_to amines_path
+    else
+      render "home", status: :unprocessable_entity
+    end
+  end
+
+  def user_index
+    @amines = Amine.where(user: current_user)
+  end
+
+  private
+
+  def amine_params
+    params.require(:amine).permit(:name, :price, :description, :mood_id, :owner_id)
+  end
+end
