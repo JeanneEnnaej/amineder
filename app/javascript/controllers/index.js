0fd088a5b729348172f71818ba7ed1a5ef4e503a
Alexis Barreirinho <alexis@barreirinho.com>
2022-08-24 18:40:16 +0200

    ajout bouton accepter / decline dans account reservation

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import DashboardCardsController from "./dashboard_cards_controller"
+application.register("dashboard-cards", DashboardCardsController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
