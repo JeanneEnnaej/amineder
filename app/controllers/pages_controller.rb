4b0fbe23b6b19b8cc7af4469be98dd243c0bce17
amlds.dev@gmail.com <amlds.dev@gmail.com>
2022-08-26 14:10:25 +0200

    finition home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,11 @@ skip_before_action :authenticate_user!, only: :home
   def home
     tous_amines = Amine.all.shuffle
     @amines = []
-    3.times do
+    4.times do
       @amines << tous_amines.pop
     end
     @amine = Amine.where("")
+    @reservation = Reservation.new
   end
 
   def test
